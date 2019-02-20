920b187e34fa14e7c6251b28ca32e7220a0dd837
Bharkavi-kannnan2000 <42996759+Bharkavi-kannnan2000@users.noreply.github.com>
2019-02-20 14:06:20 +0530

    Create prime or not.py

---
+++ prime or not.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number to check:\n")
+count=0
+for i in rage(2,a):
+if(a%i++0):
+count=count+1
+if(count==0):
+print("yes,it is a prime number")
+else:
+print("no,it is not a prime number")
