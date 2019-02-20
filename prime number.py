1b8407bb49c7b9778fc46224ef9e371bb85bc07a
Bharkavi-kannnan2000 <42996759+Bharkavi-kannnan2000@users.noreply.github.com>
2019-02-20 14:27:06 +0530

    Create prime number.py

---
+++ prime number.py
@@ -0,0 +1,12 @@
+def main():
+lower=int(input(""))
+upper=int(input(""))
+for i in range(loer,upper+1):
+if i>1:
+for i in range(2,i):
+if(i%a==0):
+break
+else:
+print(i)
+if_name_=='_main_':
+main()
